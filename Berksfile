f5fdcce146e4c0cdaedf5c1be15da81770ce941b
christian.schwartz@gmail.com <christian.schwartz@gmail.com>
2013-07-19 12:10:42 +0200

    added berkshelf and dependency on pypy

---
+++ Berksfile
@@ -0,0 +1,3 @@
+site :opscode
+
+cookbook 'pypy', git: 'https://github.com/michaelklishin/pypy-chef-cookbook.git'
--- provision.sh
+++ provision.sh
@@ -5,7 +5,8 @@ if ! test -f "$CHEF"; then
   apt-get update
   apt-get --force-yes -fuy -o Dpkg::Options::="--force-confnew" dist-upgrade
   apt-get install ruby1.9.1 ruby1.9.1-dev make
-  sudo gem1.9.1 install --no-rdoc --no-ri chef
+  sudo gem1.9.1 install --no-rdoc --no-ri chef berkshelf
+  berks install
 fi
 
 $CHEF -c solo.rb -j solo.json
