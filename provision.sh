a330ab4e0d11992352a1c0513fade3b59131d20d
christian.schwartz@gmail.com <christian.schwartz@gmail.com>
2013-07-19 14:03:10 +0200

    updated provision and solo.json

--- provision.sh
+++ provision.sh
@@ -2,12 +2,12 @@ CHEF=/usr/local/bin/chef-solo
 
 if ! test -f "$CHEF"; then
   export DEBIAN_FRONTEND=noninteractive
-  apt-get update
-  apt-get --force-yes -fuy -o Dpkg::Options::="--force-confnew" dist-upgrade
-  apt-get install ruby1.9.1 ruby1.9.1-dev make libxml2-dev libxslt1-dev
+  sudo apt-get update &
+  apt-get --force-yes -fuy -o Dpkg::Options::="--force-confnew" dist-upgrade &
+  apt-get install ruby1.9.1 ruby1.9.1-dev make libxml2-dev libxslt1-dev &
   gem1.9.1 install --no-rdoc --no-ri chef berkshelf
 fi
 
-berks install
+berks install --path cookbooks/
 
-$CHEF -c solo.rb -j solo.json
+sudo $CHEF -c solo.rb -j solo.json
